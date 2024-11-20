9131f08e4f54ad51112effcef55685e11c5ad688
PooranaSelvan <pooranaselvan67@gmail.com>
2024-11-20 20:26:45 +0530

    Fixed Login Issue?

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,7 +4,7 @@ const generateToken = async (res,userId) => {
     const token = jwt.sign({userId}, process.env.JWT_SECRET, { expiresIn: '30d' });
 
     res.cookie("jwt", token, {
-        secure: true,
+        secure: false,
         sameSite: "none",
         maxAge: 30 * 24 * 60 * 60 * 1000
     });
